01225846d3587959537c8eaa565e2ffcc85a93e1
Zarkyoz <alexandre.nickles@gmail.com>
2020-06-18 23:05:32 +0200

    modif css et js button

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // création de l'environement du chrono dans un article
+var main = document.querySelector('main');
 var cadreChrono = document.getElementById('cadreChrono');
 var display = document.createElement('section');
 var partieBtn = document.createElement('section');
 var partieBtnUn = document.getElementById('partieBtnUn')
 var partieBtnDeux = document.getElementById('partieBtnDeux')
+var flashText = document.getElementById('flashText')
 
 cadreChrono.appendChild(display);
 display.id = "display";
@@ -29,15 +31,30 @@ refresh.textContent = "Rafraichir"
 
 // Quand on clique sur le bouton validation du formulaire
 var validation = document.getElementById('validation')
-validation.addEventListener('click', function(){
+validation.addEventListener('click', function () {
     tempsActif = document.form.inputActif.value;
     tempsRepos = document.form.inputRepos.value;
-    repos = tempsRepos;
-    temps = tempsActif;
-    chrono = temps;
-    display.textContent = chrono;
-    display.style.backgroundColor = "red"
-    // partieBtnUn.replaceChild(refresh, validation)
+    if (tempsActif || tempsRepos) {
+        repos = tempsRepos;
+        temps = tempsActif;
+        chrono = temps;
+        display.textContent = chrono;
+        // main.style.backgroundColor = "red"
+        partieBtnDeux.appendChild(btnStart)
+        partieBtnUn.style.visibility = "hidden"
+        partieBtnDeux.style.visibility = "visible"
+    } else {
+        flashText.textContent = "Veuillez remplir au moins un des deux champs"
+        flashText.style.backgroundColor = 'blue';
+        flashText.style.color = 'white';
+        setTimeout(function () {
+            flashText.style.textAlign = "center";
+            flashText.textContent = "";
+            flashText.style.backgroundColor = 'white';
+            flashText.style.color = 'black';
+        }, 3000)
+
+    }
 })
 
 // refresh.addEventListener('click', function(){
@@ -57,117 +74,122 @@ var changement = new Audio()
 changement.src = "Audio/changement.mp3"
 display.textContent = chrono;
 
+// bouton image lecture/pause/stop
+var btnStart = new Image(75, 75)
+btnStart.src = "images/start.png"
+var btnPause = new Image(75, 75)
+btnPause.src = "images/pause.png"
+
 
 //  création bouton start/pause ,stop, reinitialiser
 var btnStart = document.createElement('button');
 btnStart.textContent = "Start";
-var btnStop = document.createElement('button');
-btnStop.textContent = "Stop";
-partieBtnDeux.appendChild(btnStart);
-
+btnStart.classList = "btnAction"
+var btnPause = document.createElement('button');
+btnPause.textContent = "Pause";
+btnPause.classList = "btnAction"
+// partieBtnDeux.appendChild(btnStart);
 
 
 
 // quand on clique sur "Start", "Start" et remplacer par "Stop" 
 var btnAct = 0;
-var mode = 1 ;
+var mode = 1;
 btnStart.addEventListener('click', btnStartAct)
 function btnStartAct() {
-    partieBtnUn.removeChild(validation)
-    if(mode == 1){
-        display.style.backgroundColor = "green"
-    }else if(mode == 2){
-        display.style.backgroundColor = "blue"
+    // partieBtnUn.removeChild(validation)
+    if (mode == 1) {
+        main.style.backgroundColor = "green"
+    } else if (mode == 2) {
+        main.style.backgroundColor = "blue"
     }
-    
+
     function chronoTime() {
-        if(mode == 1){
-            chrono--       
+        if (mode == 1) {
+            chrono--
             display.textContent = chrono;
             // quand j'arrive à 0 le chrono ce met en mode repos "bleu" {mode ="2"}
             switch (chrono) {
                 case 3: prevention.play()
-                    
+
                     break;
                 case 2: prevention.play()
-                    
+
                     break;
                 case 1: prevention.play()
-                    
+
                     break;
                 case 0:
-                        prevention.pause()
-                        prevention.currentTime = 0
-                        changement.play();
-                
+                    prevention.pause()
+                    prevention.currentTime = 0
+                    changement.play();
+
                     break;
                 case -1:
-                        mode = 2; 
-                        chrono = repos;
-                        display.textContent = chrono;
-                        display.style.backgroundColor = "blue";
+                    mode = 2;
+                    chrono = repos;
+                    display.textContent = chrono;
+                    main.style.backgroundColor = "blue";
                     break;
                 default:
                     break;
             }
-        }else if(mode == 2){
+        } else if (mode == 2) {
             chrono--
             display.textContent = chrono;
             // quand j'arrive à 0 le chrono ce met en mode action "vert" {mode ="1"}
             switch (chrono) {
-                case 3:prevention.play();
-                    
+                case 3: prevention.play();
+
                     break;
                 case 2: prevention.play()
-                    
+
                     break;
                 case 1: prevention.play()
-                    
+
                     break;
                 case 0:
-                        prevention.pause()
-                        prevention.currentTime = 0
-                        changement.play();
-                
+                    prevention.pause()
+                    prevention.currentTime = 0
+                    changement.play();
+
                     break;
                 case -1:
-                        mode = 1; 
-                        chrono = temps;
-                        display.textContent = chrono;
-                        display.style.backgroundColor = "green";
+                    mode = 1;
+                    chrono = temps;
+                    display.textContent = chrono;
+                    main.style.backgroundColor = "green";
                     break;
                 default:
                     break;
             }
         }
-        }
-        timer = setInterval(chronoTime, vitesseChrono)
-    
-        partieBtnDeux.replaceChild(btnStop, btnStart);
-            if (btnAct === 0) {
-                btnAct = 2;
-            } else if (btnAct == 1) {
-                btnAct = 2;
-                partieBtnDeux.removeChild(btnInit);
-            }
+    }
+    timer = setInterval(chronoTime, vitesseChrono)
+
+    partieBtnDeux.replaceChild(btnPause, btnStart);
+    if (btnAct === 0) {
+        btnAct = 2;
+    } else if (btnAct == 1) {
+        btnAct = 2;
+        partieBtnDeux.removeChild(btnStop);
+    }
+    cadreChrono.style.border = "none"
 }
 
 
 
-
-
-
-
-
-
-var btnInit = document.createElement('button');
-btnInit.textContent = "Réinitialisation";
+// var btnStop = new Image(75, 75)
+// btnStop.src ="images/stop.png"
+var btnStop = document.createElement('button');
+btnStop.textContent = "Stop";
+btnStop.classList = "btnAction"
 
 // quand on clique sur "Stop", "Stop" et remplacer par "Start"
-btnStop.addEventListener('click', btnStopAct)
-function btnStopAct() {
+btnPause.addEventListener('click', btnPauseAct)
+function btnPauseAct() {
     partieBtnUn.appendChild(validation)
-    if(prevention.play() || changement.play()){
+    if (prevention.play() || changement.play()) {
         prevention.pause()
         prevention.currentTime = 0;
         changement.pause()
@@ -175,24 +197,27 @@ function btnStopAct() {
     }
     if (btnAct == 2) {
         btnAct = 1;
-        partieBtnDeux.replaceChild(btnStart, btnStop);
-        partieBtnDeux.appendChild(btnInit);
+        partieBtnDeux.replaceChild(btnStart, btnPause);
+        partieBtnDeux.appendChild(btnStop);
     }
-        display.style.backgroundColor = "red";
-        clearInterval(timer);
+    main.style.backgroundColor = "red";
+    clearInterval(timer);
 }
 
-// quand j'appuie sur "reinitialiser" "btnInit" disparait, "chrono" reviens à 30 et "btnAct" à 0 
-btnInit.addEventListener('click', btnInitAct)
-function btnInitAct() {
+// quand j'appuie sur "reinitialiser" "btnStop" disparait, "chrono" reviens à 30 et "btnAct" à 0 
+btnStop.addEventListener('click', btnStopAct)
+function btnStopAct() {
+    partieBtnUn.style.visibility = "visible"
+    partieBtnDeux.style.visibility = "hidden"
     document.form.inputActif.value = '';
     document.form.inputRepos.value = '';
-    partieBtnDeux.removeChild(btnInit);
+    partieBtnDeux.removeChild(btnStop);
     btnAct = 0;
     mode = 1;
     chrono = ""
     display.textContent = "Timer";
-    display.style.backgroundColor="white"
+    main.style.backgroundColor = "white"
+    cadreChrono.style.border = "solid purple"
 }
 
 
